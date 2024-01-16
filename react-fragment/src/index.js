91736733dfac1d453d3e093d80073b153f9315b6
Sneha-Shrivastav <shrivastavasneha04@gmail.com>
2024-01-17 00:04:22 +0530

    react-fragment

---
+++ react-fragment/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.Fragment>
+    <h1>Hello World!!</h1>
+    <p>My name is Sneha</p>
+    <h2>I am a Software Developer</h2>
+  </React.Fragment>,
+  document.getElementById("root")
+);
+
