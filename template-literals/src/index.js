73b5647705266337162f85352e85048f1c8775d1
Sneha-Shrivastav <shrivastavasneha04@gmail.com>
2024-02-05 22:25:45 +0530

    template-literals

---
+++ template-literals/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const fname = "Sneha"
+const lname = "Shrivastava"
+
+ReactDOM.render(
+  <>
+  <h1>{`My name is ${fname} ${lname}`}</h1>
+  </>,
+  document.getElementById('root')
+)
