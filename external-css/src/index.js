5fbfa8a02850d20cfc89febf6b23e019a95a1a0c
Sneha-Shrivastav <shrivastavasneha04@gmail.com>
2024-02-07 23:04:45 +0530

    external-css

---
+++ external-css/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+
+const name = "Sneha"
+const img1 = "https://picsum.photos/200/300"
+const img2 = "https://picsum.photos/250/300"
+const img3 = "https://picsum.photos/300/300"
+const link1 = "https://picsum.photos"
+
+
+ReactDOM.render(
+  <>
+    <h1 className='heading'>My name is {name}</h1>                                                    
+    <div className='img_div'>                                                        
+      <img src={img1} alt="random Images" />
+      <img src={img2} alt="random Images" />
+      <a href={link1} target='_blank'>
+        <img src={img3} alt="random Images" />
+      </a>
+    </div>
+  </>,
+  document.getElementById('root')
+);
