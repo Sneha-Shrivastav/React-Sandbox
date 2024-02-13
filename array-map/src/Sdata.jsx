dcc6d738e3605fd78a2a5f67c657bfac57506096
Sneha-Shrivastav <shrivastavasneha04@gmail.com>
2024-02-13 22:47:01 +0530

    array-map

---
+++ array-map/src/Sdata.jsx
@@ -0,0 +1,35 @@
+const Sdata = [
+    {
+        sname: "The Vampire Diaries",
+        imgsrc: "https://wallpapercave.com/wp/wp12630943.jpg",
+        title: "A Netflix Original Series",
+        link: "https://www.netflix.com/watch/70212837?trackId=255824129"
+    },
+    {
+        sname: "Stranger Things",
+        imgsrc: "https://wallpapercave.com/uwp/uwp3670874.jpeg",
+        title: "A Netflix Original Series",
+        link: "https://www.netflix.com/watch/80077368?source=35"
+    },
+    {
+        sname: "F.R.I.E.N.D.S",
+        imgsrc: "https://wallpapercave.com/wp/wp4112166.jpg",
+        title: "A Netflix Original Series",
+        link: "https://www.netflix.com/watch/70273997?trackId=255824129&tctx=0%2C0%2Ccfd1a60d-9d82-402e-9a7a-777483998718-191569218%2Ccfd1a60d-9d82-402e-9a7a-777483998718-191569218%7C2%2Cunknown%2C%2C%2CtitlesResults%2C70153404%2CVideo%3A70273997%2CdetailsPageEpisodePlayButton"
+
+    },
+    {
+        sname: "Mismatched",
+        imgsrc: "https://wallpapercave.com/wp/wp8268982.jpg",
+        title: "A Netflix Original Series",
+        link: "https://www.netflix.com/watch/81089276?source=35"
+    },
+    {
+        sname: "WEDNESDAY",
+        imgsrc: "https://wallpapercave.com/wp/wp11784162.jpg",
+        title: "A Netflix Original Series",
+        link: "https://www.netflix.com/watch/81257204?source=35"
+    }
+]
+
+export default Sdata;
---
+++ array-map/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './Card';
+import './index.css'
+import Sdata from './Sdata';
+
+
+
+ReactDOM.render(
+  <>
+    <h1 className='heading_style'>Sneha's top 5 Netflix Pick</h1>
+    {Sdata.map((val) => {
+      return(
+        <Card
+      imgsrc={val.imgsrc}
+      title={val.title}
+      sname={val.sname}
+      link={val.link}
+    />
+      )
+    })}
+  </>, document.getElementById('root')
+);
+
