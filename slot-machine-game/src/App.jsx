adf201853590b0e5591f046a0bf591fd85b0d004
Sneha-Shrivastav <shrivastavasneha04@gmail.com>
2024-02-18 23:31:54 +0530

    Slot machine Game

---
+++ slot-machine-game/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import SlotM from './SlotMach';
+
+
+const App = () => {
+  return (
+    <>
+      <h1 className='heading_style'> 🎰Welcome to <span style={{ fontWeight: 'bold' }}>Slot machine game</span> 🎰 </h1>
+      <div className='slotmachine'>
+        <SlotM x = '😄' y = '😄' z = '😄' />
+        <hr />
+        <SlotM x = '😄' y = '😆' z = '😁' />
+        <hr />
+        <SlotM x = '🍎' y = '🍉' z = '🍎' />
+        <hr />
+        <SlotM x = '🧁' y = '🧁' z = '🧁' />
+      </div>
+    </>
+  )
+}
+
+export default App;
