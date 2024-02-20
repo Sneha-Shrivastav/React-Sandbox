0aba5ebe0b5b2b5aa05c75362ec2b8640c2da7f0
Sneha-Shrivastav <shrivastavasneha04@gmail.com>
2024-02-20 22:34:18 +0530

    react-current-time

---
+++ react-get-time/src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+const App = () => {
+
+  const state = useState()
+
+  let newTime = new Date().toLocaleTimeString();
+
+  const[cTime, setCtime] = useState(newTime)
+
+  const UpdateTime = () => {
+    newTime = new Date().toLocaleTimeString();
+    setCtime(newTime)
+  }
+
+  return(
+    <>
+      <h1>{cTime}</h1>
+      <button onClick={UpdateTime}>Get Time</button>
+    </>
+  )
+
+}
+
+export default App;
